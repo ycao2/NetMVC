47005f4cd9b538135da3b37e44d95f3c1cbd5356
Yang Cao <ycao2@luc.edu>
2016-03-30 16:24:59 -0500

    Solution Uploaded

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+
+            return View(employees);
+        }
+
+    }
+}
